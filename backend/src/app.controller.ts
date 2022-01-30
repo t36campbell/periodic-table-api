bff85c48888e0c7bc6cfb48192b7127bea91939a
t36campbell <campbell3636@yahoo.com>
2022-01-29 23:23:43 -0500

    reinit project with angular and prisma

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Element } from '@prisma/client';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('element')
+  async createUser(@Body() data: Element): Promise<Element> {
+    return this.appService.createElement(data);
   }
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
+import { Element } from '@prisma/client';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  constructor(private prisma: PrismaService) {}
+
+  async createElement(data: Element): Promise<Element> {
+    return this.prisma.element.create({data})
   }
+
 }
