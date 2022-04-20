f79d58d075c2bbe6b34fabc913c022ddf58f36c1
t36campbell <campbell3636@yahoo.com>
2022-04-20 16:42:06 -0400

    figured out hover animation

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,28 +1,31 @@
-import { AfterViewInit, Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent {
+  
+@HostListener('mousemove', ['$event']) onMouseMove(event: any) {
+  this.mousePosition = { x: event.clientX, y: event.clientY}
+}
+  
+mousePosition: {x:number, y:number} ={ x: 0, y:0 }
 
-  @ViewChild('elementMask') elementMask: any
+constructor() { }
 
-constructor(private renderer:Renderer2) { }
-
-  ngAfterViewInit(): void {
-    const elementMask = this.elementMask as ElementRef
-    const series = Array.prototype.slice.call(elementMask.nativeElement.nextSibling.lastChild.children)
-      series.forEach((child: any, index: number) => {
-        console.log(index,child)
-        // this.renderer.addClass(child.firstChild, 'element')
-        const position = child.firstChild.getBoundingClientRect();
-        console.log(child.firstChild.id,position)
-      });
+  mouseIntersect(id: string): boolean {
+    const element = document.getElementById(id)
+    const series = element?.parentElement
+    const seriesElements = Array.prototype.slice.call(series?.children)
+    const elementBounds: DOMRect = seriesElements.find(child => child.id === id).getBoundingClientRect()
+    const y = this.checkBounds(this.mousePosition.y, elementBounds.top, elementBounds.bottom)
+    const x = this.checkBounds(this.mousePosition.x, elementBounds.left, elementBounds.right)
+    return y && x
   }
 
-  log(event:any): void {
-    console.log(event)
+  checkBounds(value:number, min: number, max:number): boolean {
+    return value > min && value < max
   }
 
 }
