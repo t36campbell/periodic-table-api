20c52ce95611ee6fc972c46e9a4a7ba4233407e2
t36campbell <campbell3636@yahoo.com>
2022-04-22 23:44:04 -0400

    added more seeding fn

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -2,10 +2,137 @@ import { PrismaClient } from '@prisma/client';
 
 import { PERIODIC_TABLE } from './periodic-table';
 
+const DISCOVERERS = [];
+const GROUPS = [];
+const PERIODS = [];
+const TYPES = [];
+const PHASES = [];
+const PHASE_TYPES = [];
+
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.element.createMany({ data: PERIODIC_TABLE });
+  await createElementGroups();
+  await createElementPeriods();
+  await createElementTypes();
+  await createElements();
+  await createAtomicParticles();
+  await createElectronShells();
+  await createElementProperties();
+  await createElementDetails();
+  await createElementDiscoverers();
+  await createElementPhaseTypes();
+  await createElementPhases();
+}
+
+async function createElements() {
+  await prisma.element.createMany({
+    data: PERIODIC_TABLE.map((e) => ({
+      element: e.element,
+      symbol: e.symbol,
+      groupId: e.group,
+      periodId: e.period,
+    })),
+  });
+}
+
+async function createAtomicParticles() {
+  await prisma.atomicParticles.createMany({
+    data: PERIODIC_TABLE.map((e) => ({
+      neutrons: e.neutrons,
+      protons: e.protons,
+      electrons: e.electrons,
+    })),
+  });
+}
+
+async function createElectronShells() {
+  await prisma.electronShell.createMany({
+    data: PERIODIC_TABLE.map((e) => ({
+      shells: e.neutrons,
+      valence: e.protons,
+      electrons: e.electrons,
+    })),
+  });
+}
+
+async function createElementProperties() {
+  await prisma.elementProperties.createMany({
+    data: PERIODIC_TABLE.map((e) => ({
+      atomicMass: e.atomicMass,
+      atomicRadius: e.atomicRadius,
+      electronegativity: e.electronegativity,
+      firstIonization: e.firstIonization,
+      meltingPoint: e.meltingPoint,
+      boilingPoint: e.boilingPoint,
+      isotopes: e.isotopes,
+      specificHeat: e.specificHeat,
+      density: e.density,
+    })),
+  });
+}
+
+async function createElementDetails() {
+  await prisma.elementDetails.createMany({
+    data: PERIODIC_TABLE.map((e) => ({
+      radioactive: e.radioactive,
+      natural: e.natural,
+      metal: e.metal,
+      nonmetal: e.nonmetal,
+      metalloid: e.metalloid,
+    })),
+  });
+}
+
+async function createElementDiscoverers() {
+  await prisma.elementDiscoverer.createMany({
+    data: DISCOVERERS.map((d) => ({
+      firstName: d.firstName,
+      lastName: d.lastName,
+    })),
+  });
+}
+
+async function createElementGroups() {
+  await prisma.elementGroup.createMany({
+    data: GROUPS.map((g) => ({
+      name: g.name,
+    })),
+  });
+}
+
+async function createElementPeriods() {
+  await prisma.elementPeriod.createMany({
+    data: PERIODS.map((p) => ({
+      name: p.name,
+    })),
+  });
+}
+
+async function createElementTypes() {
+  await prisma.elementType.createMany({
+    data: TYPES.map((t) => ({
+      name: t.name,
+    })),
+  });
+}
+
+async function createElementPhases() {
+  await prisma.elementPhase.createMany({
+    data: PHASES.map((t) => ({
+      typeId: t.typeId,
+      temperatureMin: t.temperatureMin,
+      temperatureMax: t.temperatureMax,
+    })),
+  });
+}
+
+async function createElementPhaseTypes() {
+  await prisma.elementPhaseType.createMany({
+    data: PHASE_TYPES.map((p) => ({
+      name: p.name,
+    })),
+  });
 }
 
 main()
