54e09fb2eb9dfb20b301f07e3e0e28cb135019bd
t36campbell <campbell3636@yahoo.com>
2022-04-25 23:21:15 -0400

    changed type to series

---
+++ backend/prisma/series.ts
@@ -0,0 +1,13 @@
+export const SERIES = [
+  'Actinide',
+  'Alkali Earth Metal',
+  'Alkali Metal',
+  'Halogen',
+  'Lanthanide',
+  'Metalloid',
+  'Metal',
+  'Noble Gas',
+  'Nonmetal',
+  'Transactinide',
+  'Transition Metal',
+];
