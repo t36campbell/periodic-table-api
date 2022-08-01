866a0e57f9f27f541c9dba07fd361b89de1ee2e1
t36campbell <campbell3636@yahoo.com>
2022-07-31 21:57:13 -0400

    models for prisma

--- backend/src/prisma/prisma.model.ts
+++ backend/src/prisma/prisma.model.ts
@@ -1,5 +1,10 @@
-import { Prisma } from '@prisma/client'
+import { Prisma } from '@prisma/client';
 
 export const selectId = {
-    id: true
+  id: true,
+};
+
+export interface SearchElement {
+  id: number;
+  name: string;
 }
