acac617fedaa4d140b96402006ed5106871b1ba6
t36campbell <campbell3636@yahoo.com>
2022-07-31 22:59:45 -0400

    fixed issues with role guards

--- backend/src/auth/guards/jwt-auth.guard.ts
+++ backend/src/auth/guards/jwt-auth.guard.ts
@@ -5,14 +5,15 @@ import { PUBLIC_KEY } from '../../core/public.decorator';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  constructor(
-    private userService: UserService,
-  ) {
+  constructor(private userService: UserService) {
     super();
   }
 
   canActivate(context: ExecutionContext) {
-    const isPublic = this.userService.checkRequirements<boolean>(context, PUBLIC_KEY)
-    return isPublic ? isPublic : super.canActivate(context);
+    const isPublic = this.userService.checkRequirements<boolean>(
+      context,
+      PUBLIC_KEY,
+    );
+    return isPublic ? true : super.canActivate(context);
   }
 }
--- backend/src/auth/guards/role.guard.ts
+++ backend/src/auth/guards/role.guard.ts
@@ -1,5 +1,4 @@
 import { Injectable, ExecutionContext, CanActivate } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
 import { CoreService } from 'src/core/core.service';
 import { ROLES_KEY } from '../../core/role.decorator';
 import { Role } from '../auth.model';
@@ -13,14 +12,14 @@ export class RoleGuard implements CanActivate {
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const requiredRoles = await this.userService.checkRequirements<Role[]>(context, ROLES_KEY)
-    // this needs to be checked 
-    if (!requiredRoles) {
-      // console.log('am i dumb?')
-      return true;
-    }    
-    const request = await this.coreService.getRequest(context)
-    const user = await this.userService.decodeUser(request)
-    return requiredRoles.every((role) => [user.role].includes(role));
+    const requiredRoles = await this.userService.checkRequirements<Role[]>(
+      context,
+      ROLES_KEY,
+    );
+    const request = await this.coreService.getRequest(context);
+    const user = await this.userService.decodeUser(request);
+    return requiredRoles
+      ? true
+      : requiredRoles.every((role) => [user.role].includes(role));
   }
 }
