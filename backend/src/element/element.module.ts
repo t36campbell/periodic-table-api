1e94294e02e9ab63370a1ba7281d951fcd9aa929
t36campbell <campbell3636@yahoo.com>
2022-07-31 22:59:34 -0400

    fixed build issues

--- backend/src/element/element.module.ts
+++ backend/src/element/element.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { ElementService } from './element.service';
 import { ElementController } from './element.controller';
 import { CoreModule } from 'src/core/core.module';
+import { UserService } from 'src/user/user.service';
 
 @Module({
   imports: [CoreModule],
   controllers: [ElementController],
-  providers: [ElementService],
+  providers: [ElementService, UserService],
   exports: [ElementService],
 })
 export class ElementModule {}
--- backend/src/prisma/prisma.model.ts
+++ backend/src/prisma/prisma.model.ts
@@ -1,5 +1,3 @@
-import { Prisma } from '@prisma/client';
-
 export const selectId = {
   id: true,
 };
